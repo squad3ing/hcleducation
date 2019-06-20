b0416debf27d20592b2a77f67f43d5bc35c1b051
prabhu alam <rudraksh.prabhu@gmail.com>
2019-06-20 12:20:20 +0530

    course summary added subha

---
+++ src/main/java/com/ing/education/dto/EnrollmentDTO.java
@@ -0,0 +1,26 @@
+package com.ing.education.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnrollmentDTO implements Serializable {
+
+
+private static final long serialVersionUID = 1L;
+
+private Long enrollmentId;
+private StudentDTO student;
+private CourseDTO course;
+private Date enrollDate;
+private String courseName;
+}
+
---
+++ src/main/java/com/ing/education/service/IEnrollmentService.java
@@ -0,0 +1,12 @@
+package com.ing.education.service;
+
+import org.springframework.stereotype.Service;
+
+import com.ing.education.dto.EnrollmentDTO;
+
+@Service
+public interface IEnrollmentService {
+
+	EnrollmentDTO getCourseSummary(long studentId);
+
+}
